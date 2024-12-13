2813479ff319a85caa329a8125539c964fe80218
William Roger <williamroger.frontend@gmail.com>
2024-12-13 00:17:24 -0300

    exibindo nome e avatar do usuário logado

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,19 +1,25 @@
 import { Heading, Text, HStack, VStack, Icon, } from "@gluestack-ui/themed";
 import { UserPhoto } from "./UserPhoto";
 import { LogOut } from "lucide-react-native";
+import { useAuth } from "@hooks/useAuth";
+import defaultUserPhotoImg from "@assets/userPhotoDefault.png";
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
       <UserPhoto 
-        source="https://github.com/williamroger.png" 
+        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg} 
         alt="Imagem do usuário" 
         w="$16"
         h="$16"
       />
       <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">Olá,</Text>
-        <Heading color="$gray100" fontSize="$md">William Roger</Heading>
+        <Heading color="$gray100" fontSize="$md">
+          {user.name}
+        </Heading>
       </VStack>
       <Icon as={LogOut} color="$gray200" size="xl" />
     </HStack>
